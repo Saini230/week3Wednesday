b3aeb92f94f29d14c3d473dc4a31d70205e0bdde
Saini230 <saini230@sheridancollege.ca>
2024-09-18 13:20:08 -0400

    merging and commit for orignm mastr

--- src/week3wednesday/Week3Wednesday.java
+++ src/week3wednesday/Week3Wednesday.java
@@ -18,7 +18,7 @@ public class Week3Wednesday {
     //Abhishek Saini
      //illustarting and fetch 
    // after merging content
-    
+    // merge and commit for original master 
     public static void main(String[] args) {
         // TODO code application logic here
     }
